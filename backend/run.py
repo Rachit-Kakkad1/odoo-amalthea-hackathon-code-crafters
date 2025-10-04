9e8f46b0f724a484c5ebc8eb032d785f7bebcbe6
Rachit-Kakkad1 <kakkadrachit1@gmail.com>
2025-10-04 16:51:01 +0530

    Delete backend directory

--- backend/run.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    # This tells uvicorn to look for the 'app' object inside the 'app/main.py' file.
-    # Running it from this script ensures Python knows where to find the 'app' module.
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
